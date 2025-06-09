b9f76d5ec2ac29d6ac2437b7bbeda276465c5f42
Exauce Bodi <exaucebodi4@gmail.com>
2025-06-09 17:56:47 +0200

    Ajout du state HomeState pour gérer les annonces et les catégories de la page d'accueil

--- lib/pages/home/homeState.dart
+++ lib/pages/home/homeState.dart
@@ -1,19 +1,42 @@
-// create a state class for the home page
+import 'package:odc_mobile_template/business/models/announcement/announcement.dart';
+import 'package:odc_mobile_template/business/models/category/category.dart';
 
-import '../../business/models/article/article.dart';
+class Homestate {
+  final bool isLoading;
+  final String? errorMessage;
+  final List<Announcement>? announcements;
+  final List<Announcement>? salesAnnouncements;
+  final List<Announcement>? exchangeAnnouncements;
+  final List<Announcement>? donationAnnouncements;
+  final List<Category>? categories;
 
-class HomeState {
-  List<Article>? articles;
-  bool? isLoading = false;
-  String? error;
-
-  HomeState({this.articles, this.isLoading, this.error});
-
-  HomeState copyWith({List<Article>? articles, bool? isLoading, String? error}) {
-    return HomeState(articles: articles ?? this.articles, isLoading: isLoading ?? this.isLoading, error: error ?? this.error);
+  Homestate({
+    this.isLoading = false,
+    this.errorMessage,
+    this.announcements,
+    this.categories,
+    this.donationAnnouncements,
+    this.exchangeAnnouncements,
+    this.salesAnnouncements
+  });
+  
+  Homestate copyWith({
+    bool? isLoading,
+    String? errorMessage,
+    List<Announcement>? announcements,
+    List<Category>? categories,
+    List<Announcement>? salesAnnouncements,
+    List<Announcement>? exchangeAnnouncements,
+    List<Announcement>? donationAnnouncements
+  }) {
+    return Homestate(
+      isLoading: isLoading ?? this.isLoading,
+      errorMessage: errorMessage ?? this.errorMessage,
+      announcements: announcements ?? this.announcements,
+      salesAnnouncements: salesAnnouncements ?? this.salesAnnouncements,
+      donationAnnouncements: donationAnnouncements ?? this.donationAnnouncements,
+      exchangeAnnouncements: exchangeAnnouncements ?? this.exchangeAnnouncements,
+      categories: categories ?? this.categories,
+    );
   }
-  
-  
 }
-
-
