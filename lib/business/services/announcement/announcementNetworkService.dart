08665fc60b12a9f82f86da2bc42f910669c30333
Exauce Bodi <exaucebodi4@gmail.com>
2025-07-31 14:58:17 +0200

    ajout de la methode pour la création d'annonce

--- lib/business/services/announcement/announcementNetworkService.dart
+++ lib/business/services/announcement/announcementNetworkService.dart
@@ -1,6 +1,8 @@
 import 'package:odc_mobile_template/business/models/announcement/announcement.dart';
+import 'package:odc_mobile_template/business/models/announcement/createAnnouncement.dart';
 
 abstract class AnnouncementNetworkService {
   Future<List<Announcement>> getAnnouncements({List<String>? operationTypes,double? price,List<String>? states,List<String>? categories});
   Future<Announcement?> getAnnouncement(int id);
+  Future<bool> createAnnouncement(CreateAnnouncement announcement,String? token);
 }
--- lib/framework/announcement/announcementNetworkServiceImpl.dart
+++ lib/framework/announcement/announcementNetworkServiceImpl.dart
@@ -69,6 +69,82 @@ class AnnouncementNetworkServiceImpl implements AnnouncementNetworkService {
     var announcement = Announcement.fromJson(data);
     return announcement;
   }
+
+  @override
+  Future<bool> createAnnouncement(
+    CreateAnnouncement announcement,
+    String? token,
+  ) async {
+    var url = '$baseUrl/announcements';
+    var request = http.MultipartRequest('POST', Uri.parse(url));
+
+    request.fields['title'] = announcement.title;
+    request.fields['description'] = announcement.description;
+    request.fields['operation_type'] = announcement.operationType;
+    request.fields['price'] = announcement.price?.toString() ?? '';
+    request.fields['state'] = announcement.state;
+    request.fields['exchange_location_address'] =
+        announcement.exchangeLocationAddress;
+    request.fields['exchange_location_lat'] =
+        announcement.exchangeLocationLat.toString();
+    request.fields['exchange_location_lng'] =
+        announcement.exchangeLocationLng.toString();
+    request.fields['category_id'] = announcement.categoryId.toString();
+    request.fields['created_by'] = announcement.created_by.toString();
+
+    // Ajout des photos si elles existent
+    for (var photoPath in announcement.photos) {
+      var file = File(photoPath);
+
+      // Détection du type MIME (ex: image/png, image/webp, etc.)
+      final mimeType = lookupMimeType(file.path); // ex: "image/png"
+      final mimeSplit = mimeType?.split('/') ?? ['image', 'jpeg'];
+
+      request.files.add(
+        await http.MultipartFile.fromPath(
+          'photos[]', // Le nom du champ dans le formulaire
+          file.path,
+          contentType: MediaType(
+            mimeSplit[0],
+            mimeSplit[1],
+          ), // Type MIME de l'image
+        ),
+      );
+    }
+    // l'en -tête Authorization si le token est fourni
+    if (token != null) {
+      request.headers['Authorization'] = 'Bearer $token';
+    }
+    request.headers['Content-Type'] = 'multipart/form-data';
+
+    // envoi de la requête
+    var streamedResponse = await request.send();
+    var response = await http.Response.fromStream(streamedResponse);
+
+    // print(response.body);
+
+    try {
+      final body = json.decode(response.body);
+
+      // Vérifie si une propriété "success" ou "message" indique une erreur
+      if (response.statusCode == 201 || response.statusCode == 200) {
+        if (body['success'] == true || body['id'] != null) {
+          print("Annonce créée avec succès");
+          return true;
+        } else {
+          print("Échec  : ${body['message'] ?? body}");
+          return false;
+        }
+      } else {
+        print("Erreur côté serveur : ${body['message'] ?? body}");
+        return false;
+      }
+    } catch (e) {
+      print("Erreur de décodage JSON : $e");
+      return false;
+    }
+  }
+
 }
 
 void main() async {
