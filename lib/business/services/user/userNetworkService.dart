5ff51d064c820382cbab73b13ead35d3ad9497a3
Exauce Bodi <exaucebodi4@gmail.com>
2025-08-07 16:56:44 +0200

    ajout de la methode pour modifier le mot de passe

--- lib/business/services/user/userNetworkService.dart
+++ lib/business/services/user/userNetworkService.dart
@@ -1,7 +1,9 @@
+import 'package:odc_mobile_template/business/models/user/profile.dart';
 import 'package:odc_mobile_template/business/models/user/registerUser.dart';
 import 'package:odc_mobile_template/business/models/user/verifyOtp.dart';
 
 import '../../models/user/authentication.dart';
+import '../../models/user/putPassword.dart';
 import '../../models/user/user.dart';
 
 abstract class UserNetworkService {
@@ -10,4 +12,6 @@ abstract class UserNetworkService {
   Future<void> registerUser(RegisterUser registerUser);
   Future<void>  verifyOtp(VerifyOtp verifyOtp);
   Future<void> resendOtp(VerifyOtp resendOtp);
+  Future<Profile?> getProfileTutor(String token);
+  Future<void> updateUserPassword(PutPassword data,String token);
 }
