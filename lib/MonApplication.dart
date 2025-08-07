db2fcfe0a5c700ce4b1b199cf777467fa5eeae3b
Exauce Bodi <exaucebodi4@gmail.com>
2025-08-07 11:42:11 +0200

    récuperation de l'utilisateur local après l'initialisation de l'application

--- lib/MonApplication.dart
+++ lib/MonApplication.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:odc_mobile_template/pages/auth/login/loginCtrl.dart';
 import './routers.dart';
 
 class MonApplication extends ConsumerStatefulWidget {
@@ -10,6 +11,17 @@ class MonApplication extends ConsumerStatefulWidget {
 }
 
 class _MonApplicationState extends ConsumerState<MonApplication> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      var ctrl = ref.read(LoginCtrlProvider.notifier);
+      ctrl.recupererUserLocal();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final routerConfig = ref.watch(routerConfigProvider);
