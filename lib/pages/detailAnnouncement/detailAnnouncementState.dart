8cdb4c9ead92ba3633583c7fd1476372b71aff58
Exauce Bodi <exaucebodi4@gmail.com>
2025-08-08 14:36:53 +0200

    ajout de l'attribut alreadyReported pour gerer l'etat de signalement d'une annonce

--- lib/pages/detailAnnouncement/detailAnnouncementState.dart
+++ lib/pages/detailAnnouncement/detailAnnouncementState.dart
@@ -5,25 +5,29 @@ class DetailAnnouncementState {
   final List<Announcement>? similarAnnouncements;
   final bool? isLoading;
   final String? errorMsg;
+  final bool alreadyReported;
 
   DetailAnnouncementState({
     this.announcement,
     this.isLoading,
     this.errorMsg,
-    this.similarAnnouncements
+    this.similarAnnouncements,
+    this.alreadyReported = false,
   });
 
   DetailAnnouncementState copyWith({
     Announcement? announcement,
     List<Announcement>? similarAnnouncements,
     bool? isLoading,
-    String? errorMsg
+    String? errorMsg,
+    bool? alreadyReported,
   }){
     return DetailAnnouncementState(
       announcement : announcement ?? this.announcement,
       isLoading: isLoading ?? this.isLoading,
       errorMsg: errorMsg ?? this.errorMsg,
-      similarAnnouncements: similarAnnouncements ?? this.similarAnnouncements
+      similarAnnouncements: similarAnnouncements ?? this.similarAnnouncements,
+      alreadyReported: alreadyReported ?? this.alreadyReported,
     );
   }
 }
